314577a553c7eddf5f9e089e77c2c418eabc11c3
Zoe Mc Inerney <zoe.nose@gmail.com>
2024-11-12 12:20:51 +0000

    update(colors): styling color layout

--- src/components/UiColors/UiColors.tsx
+++ src/components/UiColors/UiColors.tsx
@@ -3,56 +3,34 @@ import React from "react";
 import colors from '../../_styles/_colors.json';
 
 
-type ColorsType = {
-  [key: string]: string | { [key: string]: string };
-};
-
-const getColorValue = (colorName: string | number) => {
-    console.log('colors:', colors);
-    console.log('keys:', Object.keys(colors));
-    console.log('colorName:', colorName);
-    const colorValue = (colors as ColorsType)[colorName.toString().toLowerCase() as keyof ColorsType];
-    console.log('colorValue:', colorValue);
-    return `bg-${colorValue}`;
-  }
+const getColorValue = Object.assign(Object.create(null), ...Object.entries(colors).map(([key, value]) => ({ [key.toLowerCase()]: value }))) as { [key: string]: string };
 
 const colorEntries = Object.entries(colors);
 
 export const UiColors = () => {
-    return (
-      <div className="flex flex-wrap justify-center">
-        {Object.entries(colors).map(([title, color]) => {
-          if (typeof color === 'object') {
-            return (
-              <div key={title} className="w-1/2 md:w-1/4 lg:w-1/6 p-4">
-                <h2 className="text-lg font-bold mb-2">{title}</h2>
-                <div className="flex flex-wrap">
-                  {Object.entries(color).map(([shade, colorValue]) => (
-                    <div
-                      key={shade}
-                      className={`w-1/2 md:w-1/3 lg:w-1/4 p-4`}
-                      style={{ backgroundColor: colorValue }}
-                    >
-                      {shade}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            );
-          } else {
-            return (
-              <div key={title} className="w-1/2 md:w-1/4 lg:w-1/6 p-4">
-                <h2 className="text-lg font-bold mb-2">{title}</h2>
-                <div
-                  className={`p-4`}
-                  style={{ backgroundColor: color }}
-                >
-                  {title}
-                </div>
-              </div>
-            );
-          }
-        })}
-      </div>
-    );
-  };
+  return (
+    <div className="grid grid-cols-3 gap-md">
+    {Object.entries(colors).map(([title, color]) => {
+      if (typeof color === 'object') {
+        return Object.entries(color).map(([shade, colorValue]) => (
+          <div key={`${title}-${shade}`} className="flex flex-col items-center">
+            <div className="w-36 h-36 rounded-full" style={{ backgroundColor: colorValue, padding: '40px' }}>
+            </div>
+            <span className="text-md text-gray-900">{title} {shade}</span>
+            <span className="text-sm text-grey-500 text-center pt-8">{colorValue}</span>
+          </div>
+        ));
+      } else {
+        return (
+          <div key={title} className="flex flex-col items-center">
+            <div className="w-36 h-36 rounded-full" style={{ backgroundColor: color, padding: '40px' }}>
+            </div>
+            <span className="text-md text-gray-900">{title}</span>
+            <span className="text-sm text-gray-500">{color}</span>
+          </div>
+        );
+      }
+    })}
+  </div>
+  );
+};
