c99181d4f0d0a47a1a1f90df2aeed2808e225857
Zoe Mc Inerney <zoe.nose@gmail.com>
2024-11-19 22:53:00 +0000

    update(components): ui colors formatting

--- src/components/UiColors/UiColors.tsx
+++ src/components/UiColors/UiColors.tsx
@@ -3,29 +3,41 @@ import React from "react";
 import colors from "../../_styles/_colors.json";
 
 export const UiColors = () => {
+  const baseColors = Object.entries(colors).filter(([groupTitle, groupColors]) => typeof groupColors !== "object");
+  const groupedColors = Object.entries(colors).filter(([groupTitle, groupColors]) => typeof groupColors === "object");
+
   return (
-    <div className="grid grid-cols-3 gap-md">
-    {Object.entries(colors).map(([title, color]) => {
-      if (typeof color === "object") {
-        return Object.entries(color).map(([shade, colorValue]) => (
-          <div key={`${title}-${shade}`} className="flex flex-col items-center">
-            <div className="w-36 h-36 rounded-full" style={{ backgroundColor: colorValue, padding: "40px" }}>
-            </div>
-            <span className="text-md text-gray-900">{title} {shade}</span>
-            <span className="text-sm text-grey-500 text-center pt-8">{colorValue}</span>
+    <div>
+      {baseColors.length > 0 && (
+        <div className="mb-md">
+          <h2 className="text-lg text-gray-900">Base Colors</h2>
+          <div className="grid grid-cols-3 gap-md mb-md auto-cols-auto">
+            {baseColors.map(([groupTitle, colorValue]) => (
+              <div key={groupTitle} className="flex flex-col items-center">
+                <div className="w-36 h-36 rounded-full" style={{ backgroundColor: typeof colorValue === 'string' ? colorValue : '', padding: "40px" }}>
+                </div>
+                <span className="text-md text-gray-900">{groupTitle}</span>
+                <span className="text-xs text-gray-300">{typeof colorValue === 'string' ? colorValue : ''}</span>
+              </div>
+            ))}
           </div>
-        ));
-      } else {
-        return (
-          <div key={title} className="flex flex-col items-center">
-            <div className="w-36 h-36 rounded-full" style={{ backgroundColor: color, padding: "40px" }}>
-            </div>
-            <span className="text-md text-gray-900">{title}</span>
-            <span className="text-sm text-gray-500">{color}</span>
+        </div>
+      )}
+      {groupedColors.map(([groupTitle, groupColors]) => (
+        <div className="mt-md" key={groupTitle}>
+          <h2 className="text-xl text-gray-900 mb-md">{groupTitle}</h2>
+          <div className="grid grid-cols-3 gap-md">
+            {Object.entries(groupColors).map(([shade, colorValue]) => (
+              <div key={`${groupTitle}-${shade}`} className="flex flex-col items-center">
+                <div className="w-36 h-36 rounded-full" style={{ backgroundColor: colorValue, padding: "40px" }}>
+                </div>
+                <span className="text-md text-gray-900">{shade}</span>
+                <span className="text-xs text-gray-300">{colorValue}</span>
+              </div>
+            ))}
           </div>
-        );
-      }
-    })}
-  </div>
+        </div>
+      ))}
+    </div>
   );
 };
