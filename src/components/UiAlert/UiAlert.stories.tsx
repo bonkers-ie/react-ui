4c4a22eaba71581dd654a16ac6e97b4c298fe685
rebeccaOberto <109217684+rebeccaOberto@users.noreply.github.com>
2024-12-06 11:12:21 +0000

    #UI-1 update(ui-alert): ui-alert in react

---
+++ src/components/UiAlert/UiAlert.stories.tsx
@@ -0,0 +1,46 @@
+
+import type { Meta, StoryObj } from "@storybook/react";
+import { UiAlert } from "./UiAlert";
+import { UiIcon } from "../UiIcon";
+import { EAlertTypes } from "./_types";
+import React from "react";
+import { ESize } from "../../_types/sizing";
+
+const meta = {
+	title: "Components/UiAlert",
+	component: UiAlert,
+	argTypes: {
+		kind: {
+			control: {
+				type: "select",
+			},
+			options: Object.values(EAlertTypes),
+			description: "Alert Kind",
+		},
+		children: {
+			control: {
+				type: "text",
+			},
+			description: "Alert Children",
+		}
+	},
+	args: {
+		kind: EAlertTypes.PRIMARY,
+		children: "This is a default alert",
+	},
+} satisfies Meta<typeof UiAlert>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+	render: (args)=>(
+		<UiAlert
+			kind={args.kind}
+			icon={<UiIcon name={["far", "face-smile"]} size={ESize.SM}/>}
+		>
+			{args.children}
+		</UiAlert>
+	)
+};
