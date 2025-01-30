e41aad8b997ce031428db85ba3152c26fc7d6d78
rebeccaOberto <109217684+rebeccaOberto@users.noreply.github.com>
2025-01-30 11:25:24 +0000

    APP-197 update(ui-notification-badge): new design ui badge

--- src/components/UiNotificationBadge/UiNotificationBadge.tsx
+++ src/components/UiNotificationBadge/UiNotificationBadge.tsx
@@ -1,82 +1,14 @@
 
 import React from "react";
 import cx from "classnames";
-import { EBadgeOffset } from "./_types";
-
-import { EColors } from "../UiTypography";
+import { EBadgeOffset, EBadgeSizes, EBadgeTypes } from "./_types";
 
 type UiNotificationBadge = {
 	offset?: EBadgeOffset,
 	children: React.ReactNode;
-	color?: EColors;
-};
+	kind?: EBadgeTypes;
+	size?: EBadgeSizes;
 
-const colorClasses = {
-	[EColors.WHITE]: "bg-white",
-	[EColors.BLACK]: "bg-black",
-	[EColors.TRANSPARENT]: "bg-transparent",
-	[EColors.CURRENT]: "bg-current",
-
-	[EColors.PRIMARY]: "bg-primary",
-	[EColors.PRIMARY_50]: "bg-primary-50",
-	[EColors.PRIMARY_100]: "bg-primary-100",
-	[EColors.PRIMARY_200]: "bg-primary-200",
-	[EColors.PRIMARY_300]: "bg-primary-300",
-	[EColors.PRIMARY_400]: "bg-primary-400",
-	[EColors.PRIMARY_500]: "bg-primary-500",
-	[EColors.PRIMARY_600]: "bg-primary-600",
-	[EColors.PRIMARY_700]: "bg-primary-700",
-	[EColors.PRIMARY_800]: "bg-primary-800",
-	[EColors.PRIMARY_900]: "bg-primary-900",
-
-	[EColors.PRIMARY_ALT]: "bg-primary-alt",
-	[EColors.PRIMARY_ALT_300]: "bg-primary-alt-300",
-	[EColors.PRIMARY_ALT_400]: "bg-primary-alt-400",
-	[EColors.PRIMARY_ALT_500]: "bg-primary-alt-500",
-	[EColors.PRIMARY_ALT_600]: "bg-primary-alt-600",
-	[EColors.PRIMARY_ALT_700]: "bg-primary-alt-700",
-
-	[EColors.WARNING]: "bg-warning",
-	[EColors.WARNING_300]: "bg-warning-300",
-	[EColors.WARNING_400]: "bg-warning-400",
-	[EColors.WARNING_500]: "bg-warning-500",
-	[EColors.WARNING_600]: "bg-warning-600",
-	[EColors.WARNING_700]: "bg-warning-700",
-
-	[EColors.SECONDARY]: "bg-secondary",
-	[EColors.SECONDARY_300]: "bg-secondary-300",
-	[EColors.SECONDARY_400]: "bg-secondary-400",
-	[EColors.SECONDARY_500]: "bg-secondary-500",
-	[EColors.SECONDARY_600]: "bg-secondary-600",
-	[EColors.SECONDARY_700]: "bg-secondary-700",
-
-	[EColors.SECONDARY_ALT]: "bg-secondary-alt",
-	[EColors.SECONDARY_ALT_200]: "bg-secondary-alt-200",
-	[EColors.SECONDARY_ALT_300]: "bg-secondary-alt-300",
-	[EColors.SECONDARY_ALT_400]: "bg-secondary-alt-400",
-	[EColors.SECONDARY_ALT_500]: "bg-secondary-alt-500",
-	[EColors.SECONDARY_ALT_600]: "bg-secondary-alt-600",
-	[EColors.SECONDARY_ALT_700]: "bg-secondary-alt-700",
-
-	[EColors.ERROR]: "bg-error",
-	[EColors.ERROR_300]: "bg-error-300",
-	[EColors.ERROR_400]: "bg-error-400",
-	[EColors.ERROR_500]: "bg-error-500",
-	[EColors.ERROR_600]: "bg-error-600",
-	[EColors.ERROR_700]: "bg-error-700",
-
-	[EColors.ACCENT]: "bg-accent",
-	[EColors.ACCENT_300]: "bg-accent-300",
-	[EColors.ACCENT_500]: "bg-accent-500",
-	[EColors.ACCENT_600]: "bg-accent-600",
-	[EColors.ACCENT_700]: "bg-accent-700",
-
-	[EColors.ACCENT_ALT]: "bg-accent-alt",
-	[EColors.ACCENT_ALT_300]: "bg-accent-alt-300",
-	[EColors.ACCENT_ALT_400]: "bg-accent-alt-400",
-	[EColors.ACCENT_ALT_500]: "bg-accent-alt-500",
-	[EColors.ACCENT_ALT_600]: "bg-accent-alt-600",
-	[EColors.ACCENT_ALT_700]: "bg-accent-alt-700",
 };
 
 const offsetClasses = {
@@ -86,10 +18,43 @@ const offsetClasses = {
 	[EBadgeOffset.OFFSET_BOTTOM_LEFT]: "-left-xxxs -bottom-xxxs",
 };
 
+const kindClasses = {
+	[EBadgeTypes.DEFAULT]: `
+		bg-error-500
+
+	`,
+	[EBadgeTypes.PRIMARY]: `
+		bg-primary-800
+
+	`,
+
+};
+
+const sizeClasses = {
+	[EBadgeSizes.DEFAULT]: `
+		h-sm
+		min-w-sm
+		p-xxxs
+		text-xxs
+	`,
+	[EBadgeSizes.LARGE]: `
+		h-lg
+		min-w-lg
+		p-xxs
+		justify-center
+		text-2xl
+		border-2
+
+	`
+
+};
+
 export const UiNotificationBadge: React.FC<UiNotificationBadge> = ({
 	offset,
 	children,
-	color,
+	kind = EBadgeTypes.DEFAULT,
+	size = EBadgeSizes.DEFAULT
+
 }) => {
 	if (!children) {
 		return null;
@@ -99,9 +64,6 @@ export const UiNotificationBadge: React.FC<UiNotificationBadge> = ({
 		<div
 			className={ cx(
 				"notification-badge",
-				"p-xxxs",
-				"h-sm",
-				"min-w-sm",
 				"absolute",
 				"inline-flex",
 				"items-center",
@@ -109,14 +71,14 @@ export const UiNotificationBadge: React.FC<UiNotificationBadge> = ({
 				"border",
 				"border-white",
 				"text-center",
-				"text-xxs",
 				"font-bold",
 				"leading-none",
 				"text-white",
 				"whitespace-nowrap",
 				"gap-xxxs",
-				color && colorClasses[color],
-				offset && offsetClasses[offset]
+				offset && offsetClasses[offset],
+				kindClasses[kind],
+				sizeClasses[size]
 			) }
 		>
 			{ children }
